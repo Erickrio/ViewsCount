77760815cf002ece77f96678743d4fa6c8d66801
Erick Monteiro <monteiro.ck@hotmail.com>
2023-01-13 17:10:14 -0300

    create entity

---
+++ backend/src/main/java/com/erickmonteiro/views/entities/View.java
@@ -0,0 +1,67 @@
+package com.erickmonteiro.views.entities;
+import java.io.Serializable;
+import java.util.Objects;
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_view")
+public class View implements Serializable  {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Integer views;
+	
+	public View () {
+		
+	}
+
+	public View(Long id, Integer views) {
+		super();
+		this.id = id;
+		this.views = views;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getViews() {
+		return views;
+	}
+
+	public void setViews(Integer views) {
+		this.views = views;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		View other = (View) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
